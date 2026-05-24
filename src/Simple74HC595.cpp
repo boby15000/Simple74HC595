707d80c5b55d3f27d3642a8555f6d050f9ef7dbe
Boby15000 <37212351+boby15000@users.noreply.github.com>
2026-05-24 19:02:03 +0400

    Update Simple74HC595.cpp

--- src/Simple74HC595.cpp
+++ src/Simple74HC595.cpp
@@ -1,80 +1,100 @@
 #ifndef Simple74HC595_h
-#include "Simple74HC595.h"
+  #include "Simple74HC595.h"
 #endif
 
 /**
  * Constructeur
- * @pin_DS : pin d'entrée DS (correspond au pin 14 du 74HC595)
- * @pin_STCP : pin d'entrée SHCP (correspond au pin 12 du 74HC595)
- * @pin_SHCP : pin d'entrée SHCP (correspond au pin 11 du 74HC595)
- * @numberRegistre : nombre de registre à décalage (par défaut 1 , maximum 10)
-*/
-Simple74HC595::Simple74HC595(byte pin_DS, byte pin_STCP, byte pin_SHCP, byte numberRegistre){
-    pinMode(pin_DS, OUTPUT);
-    pinMode(pin_STCP, OUTPUT);
-    pinMode(pin_SHCP, OUTPUT);
-    this->_pin_DS = pin_DS;
-    this->_pin_STCP = pin_STCP;
-    this->_pin_SHCP = pin_SHCP;
-    this->_NbrPins = min(max(numberRegistre,1),10)*8;
+ * @pin_DS        : broche DS  (pin 14 du 74HC595)
+ * @pin_STCP      : broche STCP / latch (pin 12 du 74HC595)
+ * @pin_SHCP      : broche SHCP / clock (pin 11 du 74HC595)
+ * @numberRegistre : nombre de registres en daisy-chain (1 à 10)
+ */
+Simple74HC595::Simple74HC595(byte pin_DS, byte pin_STCP, byte pin_SHCP, byte numberRegistre) {
+    this->_pin_DS      = pin_DS;
+    this->_pin_STCP    = pin_STCP;
+    this->_pin_SHCP    = pin_SHCP;
+    this->_NbrRegistres = min(max(numberRegistre, (byte)1), (byte)10);
+
+    pinMode(this->_pin_DS,   OUTPUT);
+    pinMode(this->_pin_STCP, OUTPUT);
+    pinMode(this->_pin_SHCP, OUTPUT);
+
     this->SetAll(false);
 }
 
-/*
- * Applique les Etats au Entrées/Sorties 
-*/
-void Simple74HC595::RefreshRegister(){
-  // Tant que LOW les modifications ne seront pas affectés
-  digitalWrite(this->_pin_STCP, LOW);
-
-  // boucle pour affecter chaque pin des 74hc595
-  for(int i = this->_NbrPins - 1; i >=  0; i--){
-
-    //doit etre a l'etat bas pour changer de colonne plus tard
-    digitalWrite(this->_pin_SHCP, LOW);
-
-    //affecte la valeur sur le pin DS correspondant a un pin du 74hc595
-    digitalWrite(this->_pin_DS, this->_registers[i]);
-    
-    //colonne suivante
-    digitalWrite(this->_pin_SHCP, HIGH);
-
-  }
-  //applique toutes les valeurs au 74hc595
-  digitalWrite(this->_pin_STCP, HIGH);
+/**
+ * Lit l'état d'un bit dans le tableau _registers
+ */
+boolean Simple74HC595::_getBit(byte pin) {
+    return (_registers[pin / 8] >> (pin % 8)) & 0x01;
 }
 
-/*
- * Détermine l'état de la sortie précisé en paramétre sans validation 
-*/
-void Simple74HC595::SetNoUpdate(byte pin, boolean etat){
-    this->_registers[pin] = etat;
+/**
+ * Écrit l'état d'un bit dans le tableau _registers (sans appliquer)
+ */
+void Simple74HC595::_setBit(byte pin, boolean etat) {
+    if (etat)
+        _registers[pin / 8] |=  (1 << (pin % 8));
+    else
+        _registers[pin / 8] &= ~(1 << (pin % 8));
 }
 
+/**
+ * Applique les états stockés vers les 74HC595
+ */
+void Simple74HC595::RefreshRegister() {
+    byte totalPins = this->_NbrRegistres * 8;
 
-/*
- * Détermine l'état de toutes les sorties sans validation 
-*/
-void Simple74HC595::SetAllNoUpdate(boolean etat){
-    for(int i = this->_NbrPins - 1; i >=  0; i--){
-        this->_registers[i] = etat;
+    digitalWrite(this->_pin_STCP, LOW);
+
+    for (int i = totalPins - 1; i >= 0; i--) {
+        digitalWrite(this->_pin_SHCP, LOW);
+        digitalWrite(this->_pin_DS, this->_getBit(i));
+        digitalWrite(this->_pin_SHCP, HIGH);
+    }
+
+    digitalWrite(this->_pin_STCP, HIGH);
+}
+
+/**
+ * Définit l'état d'une sortie sans appliquer
+ */
+void Simple74HC595::SetNoUpdate(byte pin, boolean etat) {
+    if (pin >= this->_NbrRegistres * 8) return;
+    this->_setBit(pin, etat);
+}
+
+/**
+ * Définit l'état de toutes les sorties sans appliquer
+ */
+void Simple74HC595::SetAllNoUpdate(boolean etat) {
+    for (int i = 0; i < this->_NbrRegistres; i++) {
+        _registers[i] = etat ? 0xFF : 0x00;
     }
 }
 
-/*
- * Détermine l'état de la sortie précisé en paramétre avec validation 
-*/
-void Simple74HC595::Set(byte pin, boolean etat){
-    this->_registers[pin] = etat;
+/**
+ * Définit l'état d'une sortie et applique
+ */
+void Simple74HC595::Set(byte pin, boolean etat) {
+    if (pin >= this->_NbrRegistres * 8) return;
+    this->_setBit(pin, etat);
     this->RefreshRegister();
 }
 
-/*
- * Détermine l'état de toutes les sorties avec validation 
-*/
-void Simple74HC595::SetAll(boolean etat){
-    for(int i = this->_NbrPins - 1; i >=  0; i--){
-        this->_registers[i] = etat;
-    }
+/**
+ * Définit l'état de toutes les sorties et applique
+ */
+void Simple74HC595::SetAll(boolean etat) {
+    this->SetAllNoUpdate(etat);
+    this->RefreshRegister();
+}
+
+/**
+ * Inverse l'état d'une sortie et applique
+ */
+void Simple74HC595::Toggle(byte pin) {
+    if (pin >= this->_NbrRegistres * 8) return;
+    this->_setBit(pin, !this->_getBit(pin));
     this->RefreshRegister();
 }
